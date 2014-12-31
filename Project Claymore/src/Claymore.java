24a9d2500a82b3def654a8021c0f9c787eb89e61
Garrett Clement <Arlan40k@gmail.com>
2014-12-30 18:40:42 -0600

    Yay

--- Project Claymore/src/Claymore.java
+++ Project Claymore/src/Claymore.java
@@ -9,7 +9,7 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.tiled.TiledMap;
-
+//Hello 
 
 public class Claymore extends BasicGame { 
 	TiledMap map;
